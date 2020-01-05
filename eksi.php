6ef1b300131226c92609fb91d831bd2c8d7b34c6
H. Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2020-01-05 19:56:19 +0300

    Giriş yap ve entry gönder örnekleri

--- eksi.php
+++ eksi.php
@@ -6,10 +6,29 @@ require "eksiAPI.php";
 // Classımızı başlatalım
 $eksi = new eksi();
 
+// Gundem yazdir.
 if ($_GET["entry"] == "a") {
     // Tüm Genel'i yazdıralım.
     print_r($eksi->Gundem("a")); 
 } else {
     // Sayı ile özel giri çekelim.
     echo $eksi->Gundem(1)["baslik"];
+}
+
+// Otomatik Giriş Yap ve Entry Gönder
+
+$eksi->girisYap("foo@gmail.com", "bar");
+$entryat = $eksi->sendEntry("php", "bu entry eksi-php ile gönderildi");
+
+if ($entryat == true) {
+  echo "entry gönderildi";
+}
+
+// Manuel Giriş Yap ve Entry Gönder
+
+$eksi->setCookie("JXRCjouz3tW89/OQd4VBqGCARb9m0FZ+XIyTxpdw7KgN4Km2ZMDAbSkUBeqEHl8M3l0X+jpyLfb0o78AWeAbz3hhWLX4Y/+U=");
+$entryat = $eksi->sendEntry("php", "bu entry eksi-php ile gönderildi");
+
+if ($entryat == true) {
+  echo "entry gönderildi";
 }
