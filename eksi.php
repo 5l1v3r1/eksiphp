ae410d1377bbd27e154a879e776b7a65c05fa229
H. Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2020-01-05 21:45:53 +0300

    Update eksi.php

--- eksi.php
+++ eksi.php
@@ -18,7 +18,9 @@ if ($_GET["entry"] == "a") {
 // Otomatik Giriş Yap ve Entry Gönder
 
 $eksi->girisYap("foo@gmail.com", "bar");
-$entryat = $eksi->sendEntry("php", "bu entry eksi-php ile gönderildi");
+$entryat = $eksi->sendEntry("php", "bu entry eksi-php ile gönderildi", "foo");
+
+// https://github.com/Quiec/Eksi-PHP/wiki/Req1-%C3%87ekme
 
 if ($entryat == true) {
   echo "entry gönderildi";
@@ -27,7 +29,9 @@ if ($entryat == true) {
 // Manuel Giriş Yap ve Entry Gönder
 
 $eksi->setCookie("JXRCjouz3tW89/OQd4VBqGCARb9m0FZ+XIyTxpdw7KgN4Km2ZMDAbSkUBeqEHl8M3l0X+jpyLfb0o78AWeAbz3hhWLX4Y/+U=");
-$entryat = $eksi->sendEntry("php", "bu entry eksi-php ile gönderildi");
+$entryat = $eksi->sendEntry("php", "bu entry eksi-php ile gönderildi", "foo");
+
+// https://github.com/Quiec/Eksi-PHP/wiki/Req1-%C3%87ekme
 
 if ($entryat == true) {
   echo "entry gönderildi";
